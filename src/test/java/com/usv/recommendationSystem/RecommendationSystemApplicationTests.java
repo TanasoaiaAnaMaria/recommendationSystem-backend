c34c548c152abfec68540d0f45e4b53957ef8ba2
Tanasoaia Ana-Maria <tanasoaiaanamaria@gmail.com>
2022-09-25 20:20:55 +0300

    initial settings

---
+++ src/test/java/com/usv/recommendationSystem/RecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usv.recommendationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
